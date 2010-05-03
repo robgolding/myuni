277644b74418917d4f776bdf113d4db362326094
Rob Golding <rob@robgolding.com>
2010-05-03 10:15:56 +0100

    Added LectureManager to get next lecture from a set (Lecture.objects.next())

--- myuni/apps/lectures/models.py
+++ myuni/apps/lectures/models.py
@@ -1,11 +1,18 @@
+import datetime
 from django.db import models
 
-from myuni.apps.course.models import Module
+class LectureManager(models.Manager):
+	def next(self):
+		now = datetime.datetime.now()
+		qs = self.get_query_set().filter(given_at__gt=now).order_by('given_at')
+		return qs[0] if qs else None
 
 class Lecture(models.Model):
 	module = models.ForeignKey('course.Module')
 	room = models.ForeignKey('campus.Room')
 	given_at = models.DateTimeField()
+	
+	objects = LectureManager()
 	
 	def __unicode__(self):
 		return '%s lecture at %s' % (self.module.code, self.given_at)
